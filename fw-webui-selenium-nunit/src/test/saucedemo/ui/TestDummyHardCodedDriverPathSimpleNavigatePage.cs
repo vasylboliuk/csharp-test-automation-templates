e7b007bb089c10d0461f89583e8c1e1fa096a1fd
Vasyl Boliuk <vbboliuk@gmail.com>
2023-12-30 12:58:40 +0200

    tets: update for dummy test hardcoded path

--- fw-webui-selenium-nunit/src/test/saucedemo/ui/TestDummyHardCodedDriverPathSimpleNavigatePage.cs
+++ fw-webui-selenium-nunit/src/test/saucedemo/ui/TestDummyHardCodedDriverPathSimpleNavigatePage.cs
@@ -27,18 +27,17 @@ public class TestDummyHardCodedDriverPathSimpleNavigatePage: BaseNUnitUiTest
     [Test]
     public void TestLoginByValidUser() 
     { 
-        // new DriverManager().SetUpDriver(new ChromeConfig());
         _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
         _driver.Navigate().GoToUrl("https://www.saucedemo.com/"); 
         _driver.FindElement(By.Id("user-name")).SendKeys("standard_user"); 
         _driver.FindElement(By.Id("password")).SendKeys("secret_sauce"); 
         _driver.FindElement(By.Id("login-button")).Click(); 
         var appLogo = _driver.FindElement(By.ClassName("app_logo")).Text; 
-        var productsLable = _driver.FindElement(By.ClassName("header_secondary_container"))
+        var productsLabel = _driver.FindElement(By.ClassName("header_secondary_container"))
             .FindElement(By.XPath("./span")).Text; 
         // Validation
         Assert.That("Swag Labs", Is.EqualTo(appLogo));
-        Assert.That("Products", Is.EqualTo(productsLable));
+        Assert.That("Products", Is.EqualTo(productsLabel));
     }
     
 }
